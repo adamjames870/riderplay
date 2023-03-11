773dc049ee4d194de8fb78ae2b47389e9cac3541
Adam James <adam.james870@yahoo.co.uk>
2023-03-11 14:26:15 +0000

    Confirmed add and recall from LiteDb

---
+++ Models/ConcreteNavEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RiderTesting;
+public class ConcreteNavEvent : NavEvent
+{
+    
+    // TODO this is to be removed, only used for playing / experimenting
+    // Many methods implemented here should be pushed back up to Abstract parent
+
+    public ConcreteNavEvent(string name)
+    {
+        Name = name;
+    }
+    
+    public override TimeSpan TimeSinceLastEvent()
+    {
+        throw new NotImplementedException();
+    }
+    
+    
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,34 @@
-﻿namespace RiderTesting.ViewModels;
+﻿using LiteDB;
+
+namespace RiderTesting.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    //public string Greeting => "Found: ";
+    public string Greeting { get; set; }
+    
+    public MainWindowViewModel()
+    {
+
+        Greeting = "Found: ";
+        using (var db = new LiteDatabase("eventdb.db"))
+        {
+            
+            var col = db.GetCollection<NavEvent>("NavEvents");
+            // ConcreteNavEvent Arr = new("Arrival");
+            // ConcreteNavEvent Dep = new("Departure");
+            // ConcreteNavEvent Noon = new("Noon");
+            // col.Insert(Arr);
+            // col.Insert(Dep);
+            // col.Insert(Noon);
+            foreach (NavEvent item in col.FindAll())
+            {
+                Greeting += $"{item.Name}(id: {item.Id}), ";
+            }
+
+        }
+        
+    }
+    
+
 }
